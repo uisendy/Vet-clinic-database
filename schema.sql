04d7127bac55d866717dfeb8243217b2f92fb9f6
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-22 16:00:44 +0100

    [multiple-table]: Update: add new Tables: Species and Owners

--- schema.sql
+++ schema.sql
@@ -14,3 +14,33 @@ CREATE TABLE animals (
 
 ALTER TABLE animals 
 ADD COLUMN species VARCHAR(100);
+
+CREATE TABLE owners (
+	id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	full_name varchar(100),
+	age int
+);
+
+CREATE TABLE species (
+	id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name varchar(100)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals 
+ADD COLUMN species_id int;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id) 
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+
+ALTER TABLE animals 
+ADD COLUMN owners_id int;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (owners_id) 
+REFERENCES owners(id)
+ON DELETE CASCADE;
